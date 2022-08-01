80e5f2faa9556544e3a8c919072f9e3827f0c3c0
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-08-01 13:40:51 +0800

    light theme and dark theme complete using next-themes plugin

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else setTheme("light");
+  };
   return (
     <div className="background">
+      <p>The current theme is {theme}</p>
       <p className="text">hello world asdasdasd</p>
+      <button onClick={toggleTheme}>toggle dark/light mode</button>
     </div>
   );
 }
