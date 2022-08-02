1ad6eb4e7a09663cce5786eddddbfcbd3de26798
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-08-02 20:47:32 +0800

    login and signup UI done

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const login = () => {
-  return <div>login</div>;
+  return (
+    <div className="container">
+      <form className="form">
+        <h2>Signup</h2>
+        <label>
+          <span>Email:</span>
+          <input type="email" required />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input type="password" required />
+        </label>
+        <button className="btn">Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default login;
--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const signup = () => {
-  return <div>signup</div>;
+  return (
+    <div className="container">
+      <form className="form">
+        <h2>Signup</h2>
+        <label>
+          <span>Email:</span>
+          <input type="email" required />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input type="password" required />
+        </label>
+        <label>
+          <span>Display name:</span>
+          <input type="text" required />
+        </label>
+        <button className="btn">Sign up</button>
+      </form>
+    </div>
+  );
 };
 
 export default signup;
