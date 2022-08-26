d3059ce8a2fdd60faeefdf27c980805f39936baf
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-08-26 20:55:31 +0800

    added a svg arrowdown  on profile dp @ navbar

--- components/ProfileDropdown.jsx
+++ components/ProfileDropdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BsFillPersonFill } from "react-icons/bs";
 import { IoMdLogOut } from "react-icons/io";
+import { AiFillDownCircle } from "react-icons/ai";
 import styles from "./ProfileDropdown.module.scss";
 import Link from "next/link";
 import { useLogout } from "../hooks/useLogout";
@@ -17,6 +18,7 @@ const ProfileDropdown = () => {
         className={styles["profile-container"]}
         onClick={() => setOpen(!open)}
       >
+        <AiFillDownCircle className={styles["down-circle-svg"]} />
         {user?.photoUrl ? (
           <Image
             src={user?.photoUrl}
@@ -25,7 +27,14 @@ const ProfileDropdown = () => {
             alt="profile avatar"
           />
         ) : (
-          <BsFillPersonFill className={styles.profile} />
+          <Image
+            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+            className={styles.profile}
+            width="40"
+            height="40"
+            alt="user-profile"
+          />
+          // <BsFillPersonFill className={styles.profile} />
         )}
       </div>
 
