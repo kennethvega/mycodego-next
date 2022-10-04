2171574e4cda0ce334502ecbcf2259718c4580b2
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-10-04 13:50:08 +0800

    reverted back to commit 6fa44a59a1bd6eed2d7f80634d3a916cd71df95e

--- pages/index.js
+++ pages/index.js
@@ -66,6 +66,3 @@ export async function getServerSideProps() {
     },
   };
 }
-// check in production if this behavior is still on
-// 1. heart increment decrement is slow.
-// 2.internet disconnection in firebase
